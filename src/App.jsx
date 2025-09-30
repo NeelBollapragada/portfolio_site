b468b0b3f6f9bf7cbf8aff323325f5727e65a3be
NeelBollapragada <neelbollapragada@gmail.com>
2025-09-30 18:39:53 +0100

    added all the images, finished project listings page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import HomePage from "./pages/HomePage";
 import { Route, Routes } from "react-router-dom";
 import Projects from "./pages/Projects";
+import ProjectPage from "./pages/ProjectPage";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/projects" element={<Projects />} />
+        <Route path="/projects/:slug" element={<ProjectPage />} />
       </Routes>
     </>
   );
---
+++ src/pages/ProjectPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import ProjectInfo from "../components/ProjectInfo";
+
+const ProjectPage = () => {
+  return (
+    <>
+      <Navbar />
+      <ProjectInfo />
+    </>
+  );
+};
+
+export default ProjectPage;
